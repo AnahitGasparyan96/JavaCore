928353707e1c72e676ff16bdd7b884a93452e74b
Anahit <anul.an801@gmail.com>
2022-05-13 14:59:14 +0400

    figure example 4

--- src/homework/FigureExample2.java
+++ src/homework/FigureExample2.java
@@ -3,13 +3,13 @@ package homework;
 public class FigureExample2 {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 8; i++) {
 
             for (int j = 0; j <= i; j++) {
                 System.out.print("  ");
 
             }
-            for (int k = 3; k >= i; k--) {
+            for (int k = 5; k >= i; k--) {
                 System.out.print("* ");
 
             }
