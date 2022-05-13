ca78438d7ccf4b2af364e45ea22b78f1e5cd7510
Anahit <anul.an801@gmail.com>
2022-05-13 14:38:49 +0400

    figure example 4

---
+++ src/homework/FigureExample2.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class FigureExample2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+
+            }
+            for (int k = 3; k >= i; k--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
+
+
